f01ac516bac6b6624cd9cf767514f7a4f8440c05
Amanda <amandamoore829@gmail.com>
2017-12-20 13:32:57 -0600

    Login works using passport

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const express = require('express');
+const session = require('express-session');
+
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const {
@@ -9,15 +11,40 @@ const {
 } = require('express-validator/check');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+
 const db = require('./db/db')
 
 let app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(session({
+  secret: 'iwendapoiddnt',
+  resave: false,
+  saveUninitialized: false
+}))
+passport.use(new LocalStrategy(function(username, password, done) {
+  db.query('SELECT id, password FROM users WHERE username=?', username, function(err, results, fields) {
+    if (err) {
+      done(err, false)
+    }
+    if (results.length === 0) {
+      done(null, false)
+    }
+    const hash = results[0];
+    done(null, true)
+  });
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 hbs.registerPartials(__dirname + '/views/partials');
 
 
@@ -38,6 +65,11 @@ app.get('/profile', function(req, res) {
     title: 'This is the profile'
   })
 });
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/profile',
+  failureRedirect: '/login'
+}));
 
 
 app.post('/register', [
@@ -107,43 +139,15 @@ app.get('/login', function(req, res) {
   })
 });
 
-app.post('/login', [
-  check('username').isLength({
-    min: 4,
-    max: 45
-  }).withMessage('Username must be 4-45 characters.'), check('email').isEmail().withMessage('Invalid email address.'), check('email').isLength({
-    min: 6,
-    max: 100
-  }).withMessage('Email address must be between 4-100 characters.'), check('password').isLength({
-    min: 8,
-    max: 30
-  }).withMessage('Password must be between 8-100 characters.'),
-], (req, res) => {
-  const err = validationResult(req)
-  if (!err.isEmpty()) {
-    console.log(err.mapped());
-    res.render('login', {
-      title: "login failed",
-      errors: err.array()
-    })
-  } else {
 
-    console.log(req.body);
-    let username = req.body.username,
-      password = req.body.password
-    db.query('INSERT INTO users (username, email, password) VALUES (? , ?, ?)', [username, email, password], function(err, results, fields) {
-      if (err) {
-        res.send('There was a login error.');
-        return
-      }
-      console.log(results);
-      res.send('logged in successfully!')
-    });
-  }
-})
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function(id, done) {
+  done(null, id)
+});
 
-
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+app.listen(3004, () => {
+  console.log('listening on port 3001');
 });
